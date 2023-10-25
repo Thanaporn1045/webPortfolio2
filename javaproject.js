b9daa8964dd154f2f74e7af3ec6c9f542cbbac08
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2023-10-25 09:49:55 +0700

    Add files via upload

---
+++ javaproject.js
@@ -0,0 +1,472 @@
+window.onload = pageLoad;
+
+function pageLoad(){
+    var totop = document.getElementById("toTop");
+    totop.onclick = windowtoTop;
+    checkmouse();
+     
+ }
+ function windowtoTop()
+ {
+     window.scrollTo(0, 0);
+ }
+
+ function checkmouse()
+ {
+    var web1 = document.getElementById("web1");
+    var web2 = document.getElementById("web2");
+    var web3 = document.getElementById("web3");
+    var web4 = document.getElementById("web4");
+
+    web1.onmouseover = function()
+    {
+        web1.classList.remove("webimgsmall");
+		web1.classList.add("webimgbig");
+    }
+    web1.onmouseout = function()
+    {
+        web1.classList.remove("webimgbig");
+		web1.classList.add("webimgsmall");
+    }
+    web1.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/7M64hwIk8UcWhkkzVeblRL/Mount-Adventure-Prototype?node-id=10%3A9&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=10%3A9");
+    }
+
+
+    web2.onmouseover = function()
+    {
+        web2.classList.remove("webimgsmall");
+		web2.classList.add("webimgbig");
+    }
+    web2.onmouseout = function()
+    {
+        web2.classList.remove("webimgbig");
+		web2.classList.add("webimgsmall");
+    }
+    web2.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/7M64hwIk8UcWhkkzVeblRL/Mount-Adventure-Prototype?node-id=10%3A9&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=10%3A9");
+    }
+
+
+    web3.onmouseover = function()
+    {
+        web3.classList.remove("webimgsmall");
+		web3.classList.add("webimgbig");
+    }
+    web3.onmouseout = function()
+    {
+        web3.classList.remove("webimgbig");
+		web3.classList.add("webimgsmall");
+    }
+    web3.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/7M64hwIk8UcWhkkzVeblRL/Mount-Adventure-Prototype?node-id=10%3A9&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=10%3A9");
+    }
+
+    web4.onmouseover = function()
+    {
+        web4.classList.remove("webimgsmall");
+		web4.classList.add("webimgbig");
+    }
+    web4.onmouseout = function()
+    {
+        web4.classList.remove("webimgbig");
+		web4.classList.add("webimgsmall");
+    }
+    web4.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/7M64hwIk8UcWhkkzVeblRL/Mount-Adventure-Prototype?node-id=10%3A9&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=10%3A9");
+    }
+
+    var weba1 = document.getElementById("weba1");
+    var weba2 = document.getElementById("weba2");
+    var weba3 = document.getElementById("weba3");
+    var weba4 = document.getElementById("weba4");
+
+    weba1.onmouseover = function()
+    {
+        weba1.classList.remove("webimgsmall");
+		weba1.classList.add("webimgbig");
+    }
+    weba1.onmouseout = function()
+    {
+        weba1.classList.remove("webimgbig");
+		weba1.classList.add("webimgsmall");
+    }
+    
+
+
+    weba2.onmouseover = function()
+    {
+        weba2.classList.remove("webimgsmall");
+		weba2.classList.add("webimgbig");
+    }
+    weba2.onmouseout = function()
+    {
+        weba2.classList.remove("webimgbig");
+		weba2.classList.add("webimgsmall");
+    }
+    
+
+
+    weba3.onmouseover = function()
+    {
+        weba3.classList.remove("webimgsmall");
+		weba3.classList.add("webimgbig");
+    }
+    weba3.onmouseout = function()
+    {
+        weba3.classList.remove("webimgbig");
+		weba3.classList.add("webimgsmall");
+    }
+    
+
+    weba4.onmouseover = function()
+    {
+        weba4.classList.remove("webimgsmall");
+		weba4.classList.add("webimgbig");
+    }
+    weba4.onmouseout = function()
+    {
+        weba4.classList.remove("webimgbig");
+		weba4.classList.add("webimgsmall");
+    }
+    
+
+
+
+    var app1 = document.getElementById("app1");
+    var app2 = document.getElementById("app2");
+    var app3 = document.getElementById("app3");
+    var app4 = document.getElementById("app4");
+    var app5 = document.getElementById("app5");
+    var app6 = document.getElementById("app6");
+    var app7 = document.getElementById("app7");
+    var app8 = document.getElementById("app8");
+
+    app1.onmouseover = function()
+    {
+        app1.classList.remove("smallapp");
+		app1.classList.add("bigapp");
+    }
+    app1.onmouseout = function()
+    {
+        app1.classList.remove("bigapp");
+		app1.classList.add("smallapp");
+    }
+    app1.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/AQOt9DUaqeDwQD1X3NQFte/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2");
+    }
+
+    app2.onmouseover = function()
+    {
+        app2.classList.remove("smallapp");
+		app2.classList.add("bigapp");
+    }
+    app2.onmouseout = function()
+    {
+        app2.classList.remove("bigapp");
+		app2.classList.add("smallapp");
+    }
+    app2.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/AQOt9DUaqeDwQD1X3NQFte/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2");
+    }
+
+    app3.onmouseover = function()
+    {
+        app3.classList.remove("smallapp");
+		app3.classList.add("bigapp");
+    }
+    app3.onmouseout = function()
+    {
+        app3.classList.remove("bigapp");
+		app3.classList.add("smallapp");
+    }
+    app3.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/AQOt9DUaqeDwQD1X3NQFte/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2");
+    }
+
+    app4.onmouseover = function()
+    {
+        app4.classList.remove("smallapp");
+		app4.classList.add("bigapp");
+    }
+    app4.onmouseout = function()
+    {
+        app4.classList.remove("bigapp");
+		app4.classList.add("smallapp");
+    }
+    app4.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/AQOt9DUaqeDwQD1X3NQFte/Untitled?node-id=1%3A2&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2");
+    }
+
+    app5.onmouseover = function()
+    {
+        app5.classList.remove("smallapp");
+		app5.classList.add("bigapp");
+    }
+    app5.onmouseout = function()
+    {
+        app5.classList.remove("bigapp");
+		app5.classList.add("smallapp");
+    }
+    app5.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/Af7cR75bM21czCXcFkpVk7/Project2?node-id=2%3A4&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A4");
+    }
+
+    app6.onmouseover = function()
+    {
+        app6.classList.remove("smallapp");
+		app6.classList.add("bigapp");
+    }
+    app6.onmouseout = function()
+    {
+        app6.classList.remove("bigapp");
+		app6.classList.add("smallapp");
+    }
+    app6.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/Af7cR75bM21czCXcFkpVk7/Project2?node-id=2%3A4&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A4");
+    }
+
+    app7.onmouseover = function()
+    {
+        app7.classList.remove("smallapp");
+		app7.classList.add("bigapp");
+    }
+    app7.onmouseout = function()
+    {
+        app7.classList.remove("bigapp");
+		app7.classList.add("smallapp");
+    }
+    app7.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/Af7cR75bM21czCXcFkpVk7/Project2?node-id=2%3A4&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A4");
+    }
+    
+    app8.onmouseover = function()
+    {
+        app8.classList.remove("smallapp");
+		app8.classList.add("bigapp");
+    }
+    app8.onmouseout = function()
+    {
+        app8.classList.remove("bigapp");
+		app8.classList.add("smallapp");
+    }
+    app8.onclick = function()
+    {
+        window.open("https://www.figma.com/proto/Af7cR75bM21czCXcFkpVk7/Project2?node-id=2%3A4&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A4");
+    }
+
+
+    var char1 = document.getElementById("char1");
+    var char2 = document.getElementById("char2");
+    char1.onmouseover = function()
+    {
+        char1.classList.remove("smallchar");
+		char1.classList.add("bigchar");
+    }
+    char1.onmouseout = function()
+    {
+        char1.classList.remove("bigchar");
+		char1.classList.add("smallchar");
+    }
+    char2.onmouseover = function()
+    {
+        char2.classList.remove("smallchar");
+		char2.classList.add("bigchar");
+    }
+    char2.onmouseout = function()
+    {
+        char2.classList.remove("bigchar");
+		char2.classList.add("smallchar");
+    }
+    var char1s = document.getElementById("char1s");
+    var char2s = document.getElementById("char2s");
+    char1s.onmouseover = function()
+    {
+        char1s.classList.remove("smallchars");
+		char1s.classList.add("bigchars");
+    }
+    char1s.onmouseout = function()
+    {
+        char1s.classList.remove("bigchars");
+		char1s.classList.add("smallchars");
+    }
+    char2s.onmouseover = function()
+    {
+        char2s.classList.remove("smallchars");
+		char2s.classList.add("bigchars");
+    }
+    char2s.onmouseout = function()
+    {
+        char2s.classList.remove("bigchars");
+		char2s.classList.add("smallchars");
+    }
+    var char1m = document.getElementById("char1m");
+    var char2m = document.getElementById("char2m");
+    char1m.onmouseover = function()
+    {
+        char1m.classList.remove("smallchars");
+		char1m.classList.add("bigchars");
+    }
+    char1m.onmouseout = function()
+    {
+        char1m.classList.remove("bigchars");
+		char1m.classList.add("smallchars");
+    }
+    char2m.onmouseover = function()
+    {
+        char2m.classList.remove("smallchars");
+		char2m.classList.add("bigchars");
+    }
+    char2m.onmouseout = function()
+    {
+        char2m.classList.remove("bigchars");
+		char2m.classList.add("smallchars");
+    }
+
+
+    var v1 = document.getElementById("v1");
+    var v2 = document.getElementById("v2");
+    var v3 = document.getElementById("v3");
+    v1.onmouseover = function()
+    {
+        v1.classList.remove("smallv");
+		v1.classList.add("bigv");
+    }
+    v1.onmouseout = function()
+    {
+        v1.classList.remove("bigv");
+		v1.classList.add("smallv");
+    }
+    v1.onclick = function()
+    {
+        window.open("https://drive.google.com/file/d/1l8yg5FRAnWlJcjd-x5qZ-ZBYLDQchA5O/view?usp=sharing");
+    }
+
+    v2.onmouseover = function()
+    {
+        v2.classList.remove("smallv");
+		v2.classList.add("bigv");
+    }
+    v2.onmouseout = function()
+    {
+        v2.classList.remove("bigv");
+		v2.classList.add("smallv");
+    }
+    v2.onclick = function()
+    {
+        window.open("https://drive.google.com/file/d/1l8yg5FRAnWlJcjd-x5qZ-ZBYLDQchA5O/view?usp=sharing");
+    }
+
+    v3.onmouseover = function()
+    {
+        v3.classList.remove("smallv");
+		v3.classList.add("bigv");
+    }
+    v3.onmouseout = function()
+    {
+        v3.classList.remove("bigv");
+		v3.classList.add("smallv");
+    }
+    v3.onclick = function()
+    {
+        window.open("https://drive.google.com/file/d/1l8yg5FRAnWlJcjd-x5qZ-ZBYLDQchA5O/view?usp=sharing");
+    }
+
+    var l1 = document.getElementById("l1");
+    var l2 = document.getElementById("l2");
+    var l3 = document.getElementById("l3");
+    var l4 = document.getElementById("l4");
+    var l5 = document.getElementById("l5");
+    l1.onmouseover = function()
+    {
+        l1.classList.remove("smallline");
+		l1.classList.add("bigline");
+    }
+    l1.onmouseout = function()
+    {
+        l1.classList.remove("bigline");
+		l1.classList.add("smallline");
+    }
+    l1.onclick = function()
+    {
+        window.open("https://line.me/S/sticker/19151593");
+    }
+
+    l2.onmouseover = function()
+    {
+        l2.classList.remove("smallline");
+		l2.classList.add("bigline");
+    }
+    l2.onmouseout = function()
+    {
+        l2.classList.remove("bigline");
+		l2.classList.add("smallline");
+    }
+    l2.onclick = function()
+    {
+        window.open("https://line.me/S/sticker/19151593");
+    }
+
+    l3.onmouseover = function()
+    {
+        l3.classList.remove("smallline");
+		l3.classList.add("bigline");
+    }
+    l3.onmouseout = function()
+    {
+        l3.classList.remove("bigline");
+		l3.classList.add("smallline");
+    }
+    l3.onclick = function()
+    {
+        window.open("https://line.me/S/sticker/19151593");
+    }
+
+    l4.onmouseover = function()
+    {
+        l4.classList.remove("smallline");
+		l4.classList.add("bigline");
+    }
+    l4.onmouseout = function()
+    {
+        l4.classList.remove("bigline");
+		l4.classList.add("smallline");
+    }
+    l4.onclick = function()
+    {
+        window.open("https://line.me/S/sticker/19151593");
+    }
+
+    l5.onmouseover = function()
+    {
+        l5.classList.remove("smallline");
+		l5.classList.add("bigline");
+    }
+    l5.onmouseout = function()
+    {
+        l5.classList.remove("bigline");
+		l5.classList.add("smallline");
+    }
+    l5.onclick = function()
+    {
+        window.open("https://line.me/S/sticker/19151593");
+    }
+
+    var pagealert = document.getElementById("changepage");
+    pagealert.onclick = function()
+    {
+        alert("go to Portfolio");
+
+    }
+ }
